57f286dc9401172b8fa78954131153092da084b5
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-02-23 22:54:16 +0530

    feat: add airport-data-provider

---
+++ src/airportDataProvider.js
@@ -0,0 +1,24 @@
+const allAirportData = require('../data/airports.json');
+
+const airportData = (airportCode) => {
+
+    if(typeof(airportCode) !== 'string'  || airportCode.length !== 3){
+        return 'Invalid Data Entered';
+    }
+
+    const airportFound = allAirportData.filter( (airport) => airport.code.toLowerCase() == airportCode.toLowerCase());
+    
+    if(airportFound.length === 0)
+        return 'Airport not found';
+
+    const airportData = {
+        name : airportFound[0].name,
+        state : airportFound[0].state,
+        country : airportFound[0].country
+    }
+
+    return airportData;
+
+};
+
+console.log(airportData('AAA'));
