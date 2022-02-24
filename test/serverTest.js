a08ed164893633118e4084d62692c9660660c972
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-02-24 11:04:17 +0530

    feat: add server.json

---
+++ test/serverTest.js
@@ -0,0 +1,7 @@
+const {requestHandler} = require('../server/server');
+
+describe('Return details after obtaining from airportDataProvider file', ()=>{
+    it('Give name, state, country', ()=>{
+        
+    });
+})
